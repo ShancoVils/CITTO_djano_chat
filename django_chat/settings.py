044fd5660bcf6854a53fcc11930816b0b543f922
Shanen <pernens@mail.ru>
2021-10-22 16:53:13 +0300

    eighteen

--- django_chat/settings.py
+++ django_chat/settings.py
@@ -149,7 +149,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('redis', 6379)],
+            "hosts": [('172.20.0.1', 6379)],
         },
     },
 }
