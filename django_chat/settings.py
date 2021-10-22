8b977a9418c87204f85c9b441bbfd2fad23f2f92
Shanen <pernens@mail.ru>
2021-10-22 14:30:37 +0300

    fouth

--- django_chat/settings.py
+++ django_chat/settings.py
@@ -120,11 +120,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'chat/static/'
+# STATIC_URL = 'chat/static/'
 
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "chat/static"),
-]
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "chat/static"),
+# ]
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'chat/static'),
+)
+
+
+
 
 ASGI_APPLICATION = "django_chat.routing.application"
 
