673757567f5fe389cce988b6e041e7a13f255431
Shanen <pernens@mail.ru>
2021-10-22 15:10:32 +0300

    twelve

--- django_chat/asgi.py
+++ django_chat/asgi.py
@@ -7,13 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 import os
-
-from channels.routing import ProtocolTypeRouter
-from django.core.asgi import get_asgi_application
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chat.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
-})
+django.setup()
+application = get_default_application()
