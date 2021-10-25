f6b47aaf44454d4cf1a4af41bae2f9202458623d
Shanen <pernens@mail.ru>
2021-10-25 12:05:05 +0300

    thirty

--- django_chat/settings.py
+++ django_chat/settings.py
@@ -127,12 +127,7 @@ USE_TZ = True
 # ]
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-
-
-
+BASE_DIR = os.path.join(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
