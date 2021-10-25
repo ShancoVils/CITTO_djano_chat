fb5a68e2d87ad0ff0cd670aa7abda929239c8e32
Shanen <pernens@mail.ru>
2021-10-25 15:18:30 +0300

    chat_1.1

--- chat/static/script.js
+++ chat/static/script.js
@@ -1,4 +1,5 @@
 let chat = document.querySelector("#chat")
+let chat_perent = document.querySelector("#chat-perent")
 let input = document.querySelector("#message-input")
 // let nickname = document.querySelector("#nickname")
 let btnSubmit = document.querySelector("#btn-submit")
@@ -15,6 +16,11 @@ const webSocket = new WebSocket('ws://' + window.location.host + '/ws/chat/'+roo
 webSocket.onmessage = function(e) {
     const data = JSON.parse(e.data);
     chat.innerHTML += '<div class="msg">' + data.message + '</div>'
+    chat_perent.scrollTop = block.scrollHeight;
+    console.log("Новое сообщение")
+    chat_perent.appendChild(chat)
+ 
+
 };
  
  
