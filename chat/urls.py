2ebeda7761d718d5b43c350b49a0b059b25de670
Shanen <pernens@mail.ru>
2021-10-26 16:42:52 +0300

    chat1.7

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.hub, name='index'),
     path('chat/<str:room_name>/', views.room, name='room'),
-    path('chat-list/', views.list, name='list'),
+
 ]
