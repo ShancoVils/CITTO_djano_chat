0579aa2c98dd38184587aa68ac7c86d1ad90b1b4
Shanen <pernens@mail.ru>
2021-10-26 16:49:32 +0300

    chat1.7

--- chat/views.py
+++ chat/views.py
@@ -11,3 +11,15 @@ def room(request, room_name):
 def hub(request):
     return render(request, 'index.html')
 
+def list(request):
+    # redis_url = os.getenv('REDIS_URL')
+    # parse.uses_netloc.append('redis')
+    # url = parse.urlparse(redis_url)
+    # r = redis.Redis(host=url.hostname, port=url.port, db=0, password=url.password)
+
+    r = redis.Redis()
+    list_name = [str(key)[14:-1] for key in r.keys("*")]
+    context = {'date': list_name}
+    print(list_name)
+    
+    return render(request, 'channels-list.html',context)
