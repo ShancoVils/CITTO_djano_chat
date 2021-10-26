41991174da94596a04029bbd36bbaee46f966819
Shanen <pernens@mail.ru>
2021-10-26 15:59:28 +0300

    chat1.4

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import redis
- 
+import urllib
+import os
 def room(request, room_name):
     return render(request, 'room.html', {
         'room_name': room_name
@@ -10,7 +11,11 @@ def hub(request):
     return render(request, 'index.html')
 
 def list(request):
-    r = redis.Redis()
+    redis_url = os.getenv('REDISTOGO_URL')
+    urllib.urlparse.uses_netloc.append('redis')
+    url = urllib.urlparse.urlparse(redis_url)
+    r = redis.Redis(host=url.hostname, port=url.port, db=0, password=url.password)
+
     list_name = [str(key)[14:-1] for key in r.keys("*")]
     context = {'date': list_name}
     print(list_name)
