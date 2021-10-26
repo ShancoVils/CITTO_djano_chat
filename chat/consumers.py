2d7d1856693117c427200ff02c9c5eb3a1b64150
Shanen <pernens@mail.ru>
2021-10-26 15:48:15 +0300

    chat1.3

--- chat/consumers.py
+++ chat/consumers.py
@@ -15,7 +15,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         # Назначим пользователя в комнату
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.room_group_name = self.room_name
         # Добавляем новую комнату
         await self.channel_layer.group_add(
             self.room_group_name,
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
- 
+import redis
  
 def room(request, room_name):
     return render(request, 'room.html', {
@@ -8,3 +8,11 @@ def room(request, room_name):
 
 def hub(request):
     return render(request, 'index.html')
+
+def list(request):
+    r = redis.Redis()
+    list_name = [str(key)[14:-1] for key in r.keys("*")]
+    context = {'date': list_name}
+    print(list_name)
+    
+    return render(request, 'channels-list.html',context)
